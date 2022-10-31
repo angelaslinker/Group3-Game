a791eea1fc7c66fb31c81d85ff0bfd8318740d7f
angelaslinker <angela.r.slinker@gmail.com>
2022-10-31 14:35:37 -0700

    Added basic GUI

---
+++ input.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import simpledialog
+ 
+# Define the root window
+ROOT = tk.Tk()
+ 
+# Define input dialog
+ROOT.withdraw()
+ 
+# Define the input dialog
+USER_INP = simpledialog.askstring(title="Input Test",
+                                  prompt="Type your Name?:")
+ 
+# Print the user input
+print("Hello", USER_INP)
