81045c0eec064e6efb99a924bd838d0f4a74a700
Antonio-Saucedo <antoniojesus@byui.edu>
2022-11-22 09:06:29 -0700

    Removed unused sounds

--- game/audio_services/README.md
+++ game/audio_services/README.md
@@ -1,7 +1,5 @@
 # Sound Effects List and Sources
 
-* [aww sound by adhimahadi from Pixabay](https://pixabay.com/sound-effects/search/aww/)
 * [drumroll sound by pixabay from Pixabay](https://pixabay.com/sound-effects/search/drum%20roll/)
 * [negative beeps sound by themusicalnomad from Pixabay](https://pixabay.com/sound-effects/search/wrong/)
 * [success 1 sound by Leszek Szary from Pixabay](https://pixabay.com/sound-effects/search/finished/)
-* [yay sound by Higgs01 from Pixabay](https://pixabay.com/sound-effects/search/yay/)
--- game/audio_services/audio_sound_effect.py
+++ game/audio_services/audio_sound_effect.py
@@ -4,33 +4,26 @@
 
 import time
 from pygame import mixer
-mixer.init() # Initialize mixer
+mixer.init()  # Initialize mixer
+
 
 class AudioSoundEffect():
     def __init__(self):
         '''Initializes audio service'''
         PATH = "game/audio_services/"
-        self._aww_sound = mixer.Sound(PATH + "aww.wav")
         self._drumroll_sound = mixer.Sound(PATH + "drumroll.wav")
         self._negativeBeeps_sound = mixer.Sound(PATH + "negativeBeeps.wav")
         self._success_sound = mixer.Sound(PATH + "success.wav")
-        self._yay_sound = mixer.Sound(PATH + "yay.wav")
 
     def playSound(self, type):
         if type == 1:
-            self._aww_sound.play()
-            time.sleep(2)
-        if type == 2:
             self._drumroll_sound.play()
             time.sleep(5)
-        if type == 3:
+        if type == 2:
             self._negativeBeeps_sound.play()
             time.sleep(2)
-        if type == 4:
+        if type == 3:
             self._success_sound.play()
-            time.sleep(2)
-        if type == 5:
-            self._yay_sound.play()
             time.sleep(2)
 
 
@@ -39,5 +32,3 @@ class AudioSoundEffect():
 # play.playSound(1)
 # play.playSound(2)
 # play.playSound(3)
-# play.playSound(4)
-# play.playSound(5)
